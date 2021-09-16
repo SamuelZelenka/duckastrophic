67d5a203f90c8fbc10a0f27e74699e2ddb3fb7f4
samuelzelenka <samuel.zelenka@hotmail.com>
2021-09-16 17:04:55 +0200

    Movement fix in restart

--- Assets/Scripts/Actions/PlayerActions/Movement.cs
+++ Assets/Scripts/Actions/PlayerActions/Movement.cs
@@ -2,24 +2,24 @@ using UnityEngine;
 
 public abstract class Movement : IAction
 {
-    protected PlayerController _player;
+    protected PlayerController player { get { return GameSession.player; } }
 
     public abstract bool HoldKeyDown { get; }
     public abstract void TriggerAction();
 
-    public void Initiate(PlayerController player) => _player = player;
+    public void Initiate(PlayerController player) { }
     protected virtual void MoveDirection(Direction direction)
     {
-        float movementSpeed = _player.acceleration;
+        float movementSpeed = player.acceleration;
 
-        if (!_player.isGrounded)
+        if (!player.isGrounded)
         {
             movementSpeed = 0.3f;
         }
 
-        _player.FaceDirection = direction;
-        _player.animator.SetBool("Running", true);
-        _player.rigidbody.velocity = _player.rigidbody.velocity + Vector2.right * (float)direction * movementSpeed * Time.deltaTime;
+        player.FaceDirection = direction;
+        player.animator.SetBool("Running", true);
+        player.rigidbody.velocity = player.rigidbody.velocity + Vector2.right * (float)direction * movementSpeed * Time.deltaTime;
     }
 }
 
@@ -28,7 +28,7 @@ public class MoveLeft : Movement
     public override bool HoldKeyDown { get { return true; } }
     public override void TriggerAction()
     {
-        if (_player.rigidbody.velocity.x < _player.maxVelocity)
+        if (player.rigidbody.velocity.x < player.maxVelocity)
         {
             MoveDirection(Direction.Left);
         }
@@ -40,7 +40,7 @@ public class MoveRight : Movement
     public override bool HoldKeyDown { get { return true; } }
     public override void TriggerAction()
     {
-        if (_player.rigidbody.velocity.x < _player.maxVelocity)
+        if (player.rigidbody.velocity.x < player.maxVelocity)
         {
             MoveDirection(Direction.Right);
         }
